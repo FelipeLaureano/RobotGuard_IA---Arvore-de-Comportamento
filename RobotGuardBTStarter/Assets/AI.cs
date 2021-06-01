ad7d0f959345d4618b453a0069df009aa64f6fe4
FelipeLaureano <felipe.laureano@hotmail.com>
2021-06-01 14:02:32 -0300

    Correção de bug
    Correção de bug e atualização de comentário

--- RobotGuardBTStarter/Assets/AI.cs
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -15,7 +15,7 @@ public class AI : MonoBehaviour
     NavMeshAgent agent;
     public Vector3 destination; // The movement destination.
     public Vector3 target;      // The position to aim to.
-    float health = 100.0f;
+    float health = 100.0f; //vida
     float rotSpeed = 5.0f;
 
     float visibleRange = 80.0f;
@@ -58,15 +58,15 @@ public class AI : MonoBehaviour
     }
 
     [Task]
-    public void PickDestination(int x, int z)
+    public void PickDestination(int x, int z) //função para pegar um ponto no mapa e recebe valores do PandaBT
     {
-        Vector3 dest = new Vector3(x, 0, z);
+        Vector3 dest = new Vector3(x, 0, z); //y=0 pois robo se mantém no chão
         agent.SetDestination(dest);
         Task.current.Succeed();//Chamar o retorno
     }
 
     [Task]
-    public void MoveToDestination()
+    public void MoveToDestination() //função para se mover até o destino
     {
         if (Task.isInspected)
         {
@@ -81,23 +81,23 @@ public class AI : MonoBehaviour
     [Task]
     public void TargetPlayer()
     {
-        target = player.transform.position;
-        Task.current.Succeed();
+        target = player.transform.position; //Achar a posição do player
+        Task.current.Succeed(); //Retorno de informação
     }
 
     [Task]
     public bool Fire()
     {
         GameObject bullet = GameObject.Instantiate(bulletPrefab,
-            bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bulletSpawn.transform.position, bulletSpawn.transform.rotation); //instancia da bala
 
-        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000); //aceleração da bala
 
         return true;
     }
 
     [Task]
-    public void LookAtTarget()
+    public void LookAtTarget() //Alteração de angulo e posição para achar o alvo
     {
         Vector3 direction = target - this.transform.position;
 
@@ -116,15 +116,15 @@ public class AI : MonoBehaviour
     }
 
     [Task]
-    bool SeePlayer()
+    bool SeePlayer() //método para verificar se robo consegue ver o player e só depois continuar outros comportamentos
     {
-        Vector3 distance = player.transform.position - this.transform.position;
+        Vector3 distance = player.transform.position - this.transform.position; //vetor entre player
         RaycastHit hit;
         bool seeWall = false;
-        Debug.DrawRay(this.transform.position, distance, Color.red);
+        Debug.DrawRay(this.transform.position, distance, Color.red); //verifica  se esta olhando para direção certa
         if(Physics.Raycast(this.transform.position, distance, out hit))
         {
-            if(hit.collider.gameObject.tag == "wall")
+            if(hit.collider.gameObject.tag == "wall") //verificar se é parede
             {
                 seeWall = true;
             }
@@ -132,7 +132,7 @@ public class AI : MonoBehaviour
 
         if (Task.isInspected)
         {
-            Task.current.debugInfo = string.Format("wall={0}", seeWall);
+            Task.current.debugInfo = string.Format("wall={0}", seeWall); //apenas para teste
         }
         if(distance.magnitude < visibleRange && !seeWall)
         {
@@ -145,21 +145,22 @@ public class AI : MonoBehaviour
     }
 
     [Task]
-    bool Turn(float angle)
+    bool Turn(float angle) //método para "virar para olhar"
     {
+        //Fazer movimentação angular:
         var p = this.transform.position + Quaternion.AngleAxis(angle, Vector3.up) * this.transform.forward;
         target = p;
         return true;
     }
 
     [Task]
-    public bool IsHealthLessThan(float health)
+    public bool IsHealthLessThan(float health) //passa parametro de vida
     {
-        return this.health < health;
+        return this.health < health; //retorno da quantidade de vida
     }
 
     [Task]
-    public bool Explode()
+    public bool Explode() //método para destruir personagem e barra de vida
     {
         Destroy(healthBar.gameObject);
         Destroy(this.gameObject);
